4a1a22109cfe772076eda4a7b03bfaa98fe0a7ef
justForL <layer_james@163.com>
2016-04-20 11:42:43 +0800

    Initial Commit

---
+++ SplashAnimationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SplashAnimationDemo
+//
+//  Created by James on 16/4/20.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
